0765cf67d2448cccf5539a83983ed5f6bf060065
Daniel Gamboa <danielgamboa10@gmail.com>
2021-02-09 15:11:26 -0600

    Set up constructor inside App class

--- src/App.js
+++ src/App.js
@@ -2,7 +2,24 @@ import React from "react";
 import axios from "axios";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      username: "dgamboa",
+      user: {},
+      followers: []
+    }
+  }
+  
   render() {
+    // axios.get(`https://api.github.com/users/${this.state.username}/followers`)
+    //   .then(res => {
+    //     console.log(res);
+    //   })
+    //   .catch(err => {
+    //     console.log(err);
+    //   })
+
     return (
       <div>
         Hello
